7b16cda89a9ed6499291be57e99b566537405131
Alaric SENAT <asenat@e3r10p23.42.fr>
2018-05-09 16:10:06 +0200

    Core include file added

---
+++ malloc/malloc.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   malloc.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asenat <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/09 16:06:57 by asenat            #+#    #+#             */
+/*   Updated: 2018/05/09 16:09:23 by asenat           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MALLOC_H
+# define MALLOC_H
+
+void	free(void *ptr);
+void	*malloc(size_t size);
+void	*realloc(void *ptr, size_t size);
+
+void	show_alloc_mem();
+#endif
