01ac60b4ef1c6d4a227f879fb87798250dbc2145
Alaric Senat <asenat@student.42.fr>
2018-08-23 13:08:12 +0200

    renaming the interface file properly

--- malloc/Rules.mk
+++ malloc/Rules.mk
@@ -1,7 +1,7 @@
 ## Auto generated make tool, don't edit manually.
 malloc/$(OBJECT_DIR)/%.o: malloc/%.c
 	$(COMP) $@ $<
-malloc/$(OBJECT_DIR)/implementations.o: malloc/implementations.c \
- malloc/malloc.h ft_malloc/ft_malloc.h
+malloc/$(OBJECT_DIR)/interface.o: malloc/interface.c malloc/malloc.h \
+  ft_malloc/ft_malloc.h
 malloc/malloc.h:
 ft_malloc/ft_malloc.h:
--- malloc/Sources.mk
+++ malloc/Sources.mk
@@ -1,1 +1,4 @@
-IMPL_OBJS := malloc/$(OBJECT_DIR)/implementations.o
+## Auto generated make tool, don't edit manually.
+
+OBJ_FILES_malloc  :=  interface.o
+OBJECTS   += $(patsubst %, malloc/$(OBJECT_DIR)/%, $(OBJ_FILES_malloc))
