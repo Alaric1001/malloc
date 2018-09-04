c9e3e5bfedaca528878e5c15c386a675d0cdb2e1
Alaric Senat <asenat@student.42.fr>
2018-09-04 17:22:38 +0200

    Some protections added to callo

--- ft_malloc/ft_calloc.c
+++ ft_malloc/ft_calloc.c
@@ -3,22 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asenat <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: asenat </var/spool/mail/asenat>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/08/23 12:45:31 by asenat            #+#    #+#             */
-/*   Updated: 2018/08/23 13:03:54 by asenat           ###   ########.fr       */
+/*   Created: 2018/08/27 09:42:43 by asenat            #+#    #+#             */
+/*   Updated: 2018/09/04 17:12:06 by asenat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_malloc/ft_malloc.h"
-
+#include "ft_malloc.h"
 #include "libft/memory/memory.h"
 
-void *ft_calloc(size_t count, size_t size)
+/*
+** if the multiplication overflow return 0
+** return the result otherwise
+** TODO Insert in libft math/ section
+*/
+static size_t safe_mult(size_t n, size_t size)
 {
-	void *ret;
+	__uint128_t mult;
 
-	ret = ft_malloc(count * size);
-	ft_bzero(ret, count * size);
-	return (ret);
+	mult = (__uint128_t)n * (__uint128_t)size;
+	if(mult >> sizeof(size_t) * 8)
+		return (0);
+	return (mult);
+}
+
+void*	ft_calloc(size_t count, size_t size)
+{
+	void* ret;
+	size_t mult;
+
+	if (!(mult = safe_mult(count, size)))
+		return (NULL);
+	if (!(ret = ft_malloc(mult)))
+		return (NULL);
+	return (ft_memset(ret, '\0', mult));
 }
